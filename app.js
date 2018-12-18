23b1b54618173cc61370837b76c7489f08cc0b7b
LibLogic <hdgknsn@gmail.com>
2018-12-18 23:20:00 +0000

    Experimented with nodeType, nodeName, hasChildNodes() and cloneNode()

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-let books = document.querySelectorAll('#book-list li .name');
-console.log(books);
+const banner = document.querySelector('#page-banner');
 
-books.forEach(function(book){
-    book.textContent += ' (book title)';
-});
 
-var bookList = document.querySelector('#book-list');
-//bookList.innerHTML = '<h2>Books and more books...</h2>';
-bookList.innerHTML += '<p>This is how you add HTML</p>';
+console.log('#page-banner node type is:', banner.nodeType);
+console.log('#page-banner node name is:', banner.nodeName);
+console.log('#page-banner has child nodes:', banner.hasChildNodes());
+
+var clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner);
