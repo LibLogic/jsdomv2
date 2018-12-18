4838eb8dc1f17c8daf8acd9e241e10ab39aeca94
LibLogic <hdgknsn@gmail.com>
2018-12-18 00:59:04 +0000

    used Array.isArray(), Array.from(), Array.forEach()

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-const titles = document.getElementsByClassName('title');
+var titles = document.getElementsByTagName('li');
 
+// titles = Array.from(titles)
 console.log(Array.isArray(titles));
-console.log(Array.isArray(Array.from(titles)));
 
-Array.from(titles).forEach(function(title){
-  console.log(title);
+
+
+Array.from(titles).forEach(function(item){
+    console.log(item);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const titles = document.getElementsByClassName('title');
+
+// console.log(Array.isArray(titles));
+// console.log(Array.isArray(Array.from(titles)));
+
+// Array.from(titles).forEach(function(title){
+//   console.log(title);
+// });
