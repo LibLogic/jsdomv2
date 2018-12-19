4b66d752b92a7228d6abf118cebed5b8acef924c
LibLogic <hdgknsn@gmail.com>
2018-12-19 00:48:19 +0000

    took advantage of event bubbling, added the event listener to the parent. This way we wont need an event listener on every li we add later.

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-var btns = document.querySelectorAll('#book-list .delete');
-
-btns.forEach(function(btn){
-    btn.addEventListener('click', function(e){
-        const li = e.target.parentElement;
-        li.parentElement.removeChild(li);
-    });
-});
-
 var link = document.querySelector('#page-banner a');
 link.addEventListener('click', function(e){
     e.preventDefault();
     e.target.classList.add('hide');
-})
+});
+
+var list = document.querySelector('#book-list ul');
+list.addEventListener('click', function(e){
+    if (e.target.className == 'delete'){
+        const li = e.target.parentElement;
+        list.removeChild(li);
+    }
+});
