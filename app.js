785c85a91dc2f7414a2ea7b7bd5b341c22553cff
LibLogic <hdgknsn@gmail.com>
2018-12-20 11:02:19 +0000

    added Search Bar functionality using a keyup event

--- app.js
+++ app.js
@@ -39,3 +39,18 @@ hideBox.addEventListener('change', function(e){
     }
 });
 
+const searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', function(e){
+    const term = e.target.value.toLowerCase();
+    const books = list.getElementsByTagName('li');
+    Array.from(books).forEach(function(book){
+        const title = book.firstElementChild.textContent;
+        if(title.toLowerCase().indexOf(term) !== -1){
+            book.style.display = 'block';
+        } else {
+            book.style.display = 'none';
+        }
+    });
+});
+
+
