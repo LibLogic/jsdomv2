09a92ecc2f62b7b54b953ba25d4663d34ecb8c64
LibLogic <hdgknsn@gmail.com>
2018-12-20 12:22:47 +0000

    added tabbed content. There was an error in the css file where we were using .tab to display: none the panels.....should have been .panel.active NOT .tab.active

--- app.js
+++ app.js
@@ -53,4 +53,20 @@ searchBar.addEventListener('keyup', function(e){
     });
 });
 
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+tabs.addEventListener('click', function(e){
+    if (e.target.tagName == "LI" ){
+        const targetPanel = document.querySelector(e.target.dataset.target);
+        console.log(targetPanel);
+        panels.forEach(function(panel){
+            if(panel == targetPanel) {
+                panel.classList.add('active');
+            } else {
+                panel.classList.remove('active');
+            }
+        });
+    }
+});
+
 
